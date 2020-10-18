db9b2d0d41a407e195c10dde4b415fa2e41e814f
WAIGYIYANKYAW <waigyiyankyaw@gmail.com>
2020-10-18 17:31:43 +0630

    finish home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM
+    <Router>
+      <Switch>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/" >
+            <Header />
+            <Home />
+          </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCsTbBFGg1-yg34RBDwh2pZb3yofX9vRGE",
+  authDomain: "react--clone-4afe0.firebaseapp.com",
+  databaseURL: "https://react--clone-4afe0.firebaseio.com",
+  projectId: "react--clone-4afe0",
+  storageBucket: "react--clone-4afe0.appspot.com",
+  messagingSenderId: "774990315930",
+  appId: "1:774990315930:web:8b7045bc84f95921e23a23",
+  measurementId: "G-KWJT86FXRN"
+};
